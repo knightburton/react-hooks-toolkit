f9f42dacec840988661b9ad321b9e7d8c90af131
Imre Kiss <jakab.asd@gmail.com>
2022-01-03 18:42:29 +0100

    Add useTimeout tests

---
+++ test/useTimeout/index.ts
@@ -0,0 +1,51 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useTimeout from '../../src/useTimeout';
+
+const callback = jest.fn(() => {});
+
+describe('useTimeout', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+    jest.spyOn(global, 'clearTimeout');
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+    callback.mockClear();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  test('should do nothing when delay is null', () => {
+    const { unmount } = renderHook(() => useTimeout(callback, null));
+
+    expect(callback).not.toHaveBeenCalled();
+    unmount();
+    expect(clearTimeout).not.toHaveBeenCalled();
+  });
+
+  test('should call the callback after delay and clear the interval after unmouunt', () => {
+    const { unmount } = renderHook(() => useTimeout(callback, 200));
+
+    expect(callback).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(1000);
+    expect(callback).toHaveBeenCalled();
+    unmount();
+    expect(clearTimeout).toHaveBeenCalled();
+  });
+
+  test('should call the callback after delay change', () => {
+    const { unmount, rerender } = renderHook(({ delay }) => useTimeout(callback, delay), { initialProps: { delay: 300 } });
+
+    expect(callback).toHaveBeenCalledTimes(0);
+    jest.advanceTimersByTime(1000);
+    expect(callback).toHaveBeenCalledTimes(1);
+    rerender({ delay: 333 });
+    jest.advanceTimersByTime(333);
+    expect(callback).toHaveBeenCalledTimes(2);
+    unmount();
+    expect(clearTimeout).toHaveBeenCalled();
+  });
+});
