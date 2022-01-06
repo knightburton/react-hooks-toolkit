8355aa6f2c364579ea8eda4f909c58405a58b57f
Imre Kiss <jakab.asd@gmail.com>
2022-01-06 16:09:12 +0100

    Export useFetch from index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as useDebounce } from './useDebounce';
+export { default as useFetch } from './useFetch';
 export { default as useInterval } from './useInterval';
 export { default as usePrevious } from './usePrevious';
 export { default as useTimeout } from './useTimeout';
