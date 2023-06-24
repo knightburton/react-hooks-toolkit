b5cb4ddd1690e30c80df3038d38004ebd5592e2d
Imre Kiss <imre.kiss@one-beyond.com>
2023-06-24 13:41:45 +0200

    fix: export the debounce function hook

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as useDebounce } from './useDebounce';
+export { default as useDebounceFunction } from './useDebounceFunction';
 export { default as useFetch } from './useFetch';
 export { default as useInterval } from './useInterval';
 export { default as usePrevious } from './usePrevious';
